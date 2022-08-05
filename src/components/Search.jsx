d847d3cf2ab2a2e70d933b14064bc49a1ddccd22
Yemi Michael Omotoso <104774940+yemi-js@users.noreply.github.com>
2022-08-05 21:05:07 +0100

    Navbar done.

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className=''>
+        <input
+            className='w-full focus:border-
+             bg-gray-200 px-4 py-1 rounded-md' 
+            type="text"
+            placeholder='Search for a Country' 
+
+            />
+    </div>
+  )
+}
+
+export default Search
